5a98f33eafe92052697035db600c1794bfeb498f
Tony Ko <tony@tko.dev>
2022-03-10 15:17:04 -0500

    Add defaults

--- shared/constants/env.ts
+++ shared/constants/env.ts
@@ -1,6 +1,6 @@
-const DB_HOST = String(process.env.DB_HOST ?? '')
-const DB_PORT = Number(process.env.DB_PORT) || undefined
-const DB_USER = String(process.env.DB_USER ?? '')
-const DB_PASS = String(process.env.DB_PASS ?? '')
+const DB_HOST = String(process.env.DB_HOST ?? '') || ''
+const DB_PORT = Number(process.env.DB_PORT) || 3306
+const DB_USER = String(process.env.DB_USER ?? '') || ''
+const DB_PASS = String(process.env.DB_PASS ?? '') || ''
 
 export { DB_HOST, DB_PORT, DB_USER, DB_PASS }
